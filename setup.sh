cba249a1930fda369086b598a3d060df19a813d5
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2016-11-11 13:54:23 -0600

    Replaced hardcoded directories

--- setup.sh
+++ setup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-#Clean UP
-rm -rf download
-rm -rf services
+DOWNLOAD_DIR=download
+SERVICES_DIR=services
 
-mkdir download
-mkdir services
+#Clean UP
+rm -rf $DOWNLOAD_DIR
+rm -rf $SERVICES_DIR
+
+mkdir $DOWNLOAD_DIR
+mkdir $SERVICES_DIR
 
 echo "Downloading Spark"
-wget -q -O download/spark-2.0.1-bin-hadoop2.7.tgz http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
+wget -q -O $DOWNLOAD_DIR/spark-2.0.1-bin-hadoop2.7.tgz http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
 
 echo "Extracting Spark"
-tar -xzf download/spark-2.0.1-bin-hadoop2.7.tgz -C services/
+tar -xzf $DOWNLOAD_DIR/spark-2.0.1-bin-hadoop2.7.tgz -C $SERVICES_DIR/
 
 echo "Done - Spark"
 
 echo "Downloading Kafka"
-wget -q -O download/kafka_2.11-0.10.1.0.tgz http://ftp.wayne.edu/apache/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz
+wget -q -O $DOWNLOAD_DIR/kafka_2.11-0.10.1.0.tgz http://ftp.wayne.edu/apache/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz
 
 echo "Extracting Kafka"
-tar -xzf download/kafka_2.11-0.10.1.0.tgz -C services/
+tar -xzf $DOWNLOAD_DIR/kafka_2.11-0.10.1.0.tgz -C $SERVICES_DIR/
 
 echo "Done - Kafka"
