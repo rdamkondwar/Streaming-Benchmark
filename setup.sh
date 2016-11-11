1deaec56b6578d71f767c866f398699b0e46bf88
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2016-11-10 18:21:54 -0600

    Fixed download Kafka issue

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ tar -xzf download/spark-2.0.1-bin-hadoop2.7.tgz -C services/
 echo "Done - Spark"
 
 echo "Downloading Kafka"
-wget -q -O download/ http://ftp.wayne.edu/apache/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz
+wget -q -O download/kafka_2.11-0.10.1.0.tgz http://ftp.wayne.edu/apache/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz
 
 echo "Extracting Kafka"
 tar -xzf download/kafka_2.11-0.10.1.0.tgz -C services/
