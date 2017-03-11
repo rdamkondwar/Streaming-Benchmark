181262bfe489c7236643a9204d8579ff0dc9be1a
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2017-03-10 22:02:40 -0600

    Changing Master

--- worker_setup.sh
+++ worker_setup.sh
@@ -1,32 +1,32 @@
-MASTER="cp-1.cluster.spark-heron-pg0.utah.cloudlab.us"
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export PATH=/usr/lib/jvm/java-8-oracle/jre/bin:\$PATH" >> ~/.bashrc
+MASTER="cp-2.exp6.spark-heron-pg0.utah.cloudlab.us"
+#echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+#export PATH=/usr/lib/jvm/java-8-oracle/jre/bin:\$PATH" >> ~/.bashrc
 
-source ~/.bashrc
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+#source ~/.bashrc
+apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
  
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
-  sudo tee /etc/apt/sources.list.d/mesosphere.list
-sudo apt-get -y update
+  tee /etc/apt/sources.list.d/mesosphere.list
+apt-get -y update
  
 # Use `apt-cache showpkg mesos | grep [version]` to find the exact version.
-sudo apt-get -y install mesos
+apt-get -y install mesos
 
-sudo echo "zk://$MASTER:2181/mesos" > /etc/mesos/zk
+echo "zk://$MASTER:2181/mesos" > /etc/mesos/zk
 
-sudo service mesos-slave start
+service mesos-slave start
 
-sudo apt-get install -y python2.7 wget
+apt-get install -y python2.7 wget
  
 # NOTE: This appears to be a missing dependency of the mesos deb package and is needed
 # for the python mesos native bindings.
-sudo apt-get -y install libcurl4-nss-dev
+#sudo apt-get -y install libcurl4-nss-dev
  
 wget -c https://apache.bintray.com/aurora/ubuntu-trusty/aurora-executor_0.15.0_amd64.deb
-sudo dpkg -i aurora-executor_0.15.0_amd64.deb
+dpkg -i aurora-executor_0.15.0_amd64.deb
 
-sudo apt-get install zip libunwind-setjmp0-dev zlib1g-dev unzip -y
+#apt-get install zip libunwind-setjmp0-dev zlib1g-dev unzip -y
 
-sudo service thermos start
+service thermos start
