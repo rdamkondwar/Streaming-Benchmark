c7baccca84207df4dff7df5f6eda802c260c4e20
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2017-03-10 19:12:03 -0600

    Adding hdfs init dirs

--- scripts/spark_install.sh
+++ scripts/spark_install.sh
@@ -12,3 +12,7 @@ setup_spark() {
 setup_spark
 
 cp props/metrics.properties spark-2.0.1-bin-hadoop2.7/conf/metrics.properties
+
+mkdir /users/rohitsd/hdfs
+mkdir /users/rohitsd/hdfs_nn_dirs
+mkdir /users/rohitsd/hdfs_dn_dirs
