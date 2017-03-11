180731d9e9aa1b051f7341b83e6f903e3e90d7e2
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2017-03-11 00:23:55 -0600

    Adding aurora & mesos installer

---
+++ scripts/aurora_installer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MASTER=cp-2.exp6.spark-heron-pg0.utah.cloudlab.us
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+ 
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
+    tee /etc/apt/sources.list.d/mesosphere.list
+apt-get -y update
+ 
+# Use `apt-cache showpkg mesos | grep [version]` to find the exact version.
+apt-get -y install mesos
+
+start mesos-master
+
+wget -c https://apache.bintray.com/aurora/ubuntu-trusty/aurora-scheduler_0.15.0_amd64.deb
+sudo dpkg -i aurora-scheduler_0.15.0_amd64.deb
+
+sudo -u aurora mkdir -p /var/lib/aurora/scheduler/db
+sudo -u aurora mesos-log initialize --path=/var/lib/aurora/scheduler/db
+
+sed -i '/ZK_ENDPOINTS="127.0.0.1:2181"/c\ZK_ENDPOINTS="'$MASTER':2181"' /etc/default/aurora-scheduler
+
+sed -i '/"name": "example",/c\"name": "aurora2",' /etc/aurora/clusters.json
+
+service aurora-scheduler start
+
+wget -c https://apache.bintray.com/aurora/ubuntu-trusty/aurora-tools_0.15.0_amd64.deb
+dpkg -i aurora-tools_0.15.0_amd64.deb
+
+
